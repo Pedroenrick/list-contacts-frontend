1691829224fc5217d2f938d67c83c55ca2a461e7
Pedroenrick Felizardo <Pedro_penna22@hotmail.com>
2022-06-20 15:48:58 -0300

    terminado header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiSearchLine, RiUserAddLine } from "react-icons/ri";
 export function Header() {
   return (
     <Flex
@@ -37,7 +37,31 @@ export function Header() {
           mr="4"
           _placeholder={{ color: "gray.400" }}
         />
-        <Icon as={RiSearchLine} size="lg" />
+        <Icon as={RiSearchLine} fontSize="20" />
+
+        <Flex align="center" ml="auto">
+          <HStack
+            spacing="4"
+            mx="8"
+            pr="8"
+            py="1"
+            color="gray.300"
+            borderRightWidth={1}
+            borderColor="gray.700"
+          >
+            <Icon as={RiUserAddLine} fontSize="20" ml="4" />
+          </HStack>
+          <Flex align="center">
+            <Box mr="4" textAlign="right">
+              <Text>Pedroenrick Felizardo</Text>
+              <Text color="gray.300" fontSize="small">
+                Pedro@email.com
+              </Text>
+            </Box>
+
+            <Avatar size="md" name="Pedroenrick Felizardo" />
+          </Flex>
+        </Flex>
       </Flex>
     </Flex>
   );
